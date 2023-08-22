3d84bdb1b91b5ae1bd2ee27efa4ef97449fb8366
shielaQAQ <21121513@bjtu.edu.cn>
2023-08-22 08:06:12 -0400

    my commit about experiment a0

--- exercises/cos_arceos/ulib/axstd/src/macros.rs
+++ exercises/cos_arceos/ulib/axstd/src/macros.rs
@@ -21,3 +21,10 @@ macro_rules! println {
         $crate::io::__print_impl(format_args!("{}\n", format_args!($($arg)*)));
     }
 }
+
+#[macro_export]
+macro_rules! println_prefix {
+    ($prefix:expr, $($arg:tt)*) => {
+        $crate::print!("{}{}", $prefix, format_args!($($arg)*));
+    }
+}
