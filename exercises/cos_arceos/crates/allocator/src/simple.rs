1eeba1e7c4ef011aed83783eeac3a80889781a91
shielaQAQ <21121513@bjtu.edu.cn>
2023-08-22 09:49:34 -0400

    my commit about experiment a1

--- exercises/cos_arceos/crates/allocator/src/simple.rs
+++ exercises/cos_arceos/crates/allocator/src/simple.rs
@@ -1,48 +1,83 @@
 //! Simple memory allocation.
 //!
 //! TODO: more efficient
-
+use super::{AllocError};
 use core::alloc::Layout;
 use core::num::NonZeroUsize;
 
 use crate::{AllocResult, BaseAllocator, ByteAllocator};
 
-pub struct SimpleByteAllocator;
+pub struct SimpleByteAllocator {
+    start: usize,
+    size: usize,
+    next: usize,
+    allocations: usize,
+}
 
 impl SimpleByteAllocator {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            size: 0,
+            next: 0,
+            allocations: 0,
+        }
     }
 }
 
 impl BaseAllocator for SimpleByteAllocator {
-    fn init(&mut self, _start: usize, _size: usize) {
-        todo!();
+    fn init(&mut self, start: usize, size: usize) {
+        self.start = start;
+        self.size = size;
+        self.next = start;
+        self.allocations = 0;
     }
 
-    fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
-        todo!();
+    fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
+        if self.start + self.size == start && size > 0 {
+            self.size += size;
+            AllocResult::Ok(())
+        } else {
+            AllocResult::Err(AllocError::MemoryOverlap)
+            
+        }
     }
 }
 
 impl ByteAllocator for SimpleByteAllocator {
-    fn alloc(&mut self, _layout: Layout) -> AllocResult<NonZeroUsize> {
-        todo!();
+    fn alloc(&mut self, layout: Layout) -> AllocResult<NonZeroUsize> {
+        let size = layout.size();
+        let align = layout.align();
+
+        let aligned_next = (self.next + align - 1) & !(align - 1);
+
+        if aligned_next + size <= self.start + self.size {
+            let allocation = self.next;
+            self.next = aligned_next + size;
+            self.allocations += 1;
+            AllocResult::Ok(unsafe { NonZeroUsize::new_unchecked(allocation) })
+        } else {
+            AllocResult::Err(AllocError::NoMemory)
+            
+        }
     }
 
     fn dealloc(&mut self, _pos: NonZeroUsize, _layout: Layout) {
-        todo!();
+        self.allocations -= 1;
+        if self.allocations == 0 {
+            self.next = self.start;
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!();
+        self.size
     }
 
     fn used_bytes(&self) -> usize {
-        todo!();
+        self.next - self.start
     }
 
     fn available_bytes(&self) -> usize {
-        todo!();
+        self.size - (self.next - self.start)
     }
 }
